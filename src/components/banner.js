584d05577f1320f52471284e9f356ff7845f626f
felipesaev <felipesaev@gmail.com>
2019-12-26 12:42:12 -0300

    projetomdp

---
+++ src/components/banner.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Banner = () => { 
+  return (
+    <div className="banner">
+      <div className="container">
+        <div className="row">         
+          <div className="main-text">Mulheres de peito</div>        
+        </div>
+        <div className="scroll">
+          <span>Scroll down</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
