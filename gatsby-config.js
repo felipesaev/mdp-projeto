ad5957e75315ab33527aed2a61a1e695415a75fe
felip <felipe.costa@zappts.com>
2020-01-13 22:01:31 -0300

    mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,17 @@ module.exports = {
   plugins: [   
     `gatsby-plugin-styled-components`, 
     `gatsby-plugin-react-helmet`,
+    
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
-      },
-
-      
+      },      
+        resolve: `gatsby-plugin-mdx`,
+        options: {
+          extensions: [`.mdx`, `.md`],
+        }      
     
     },
     `gatsby-transformer-sharp`,
