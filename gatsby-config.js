1a8fcdd6468a55db9aa3c344e5094f590e634b36
Felipe <felipesaev@gmail.com>
2020-03-06 00:52:19 -0300

    adicionando o manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,13 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: `https://www.mulheresdepeito.netlify.com`,
   },
   plugins: [   
     `gatsby-plugin-styled-components`, 
     `gatsby-plugin-react-helmet`,
-    
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,    
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,8 +27,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mulheres de peito`,
+        short_name: `mdp`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -40,8 +42,10 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+     `gatsby-plugin-offline`,
   ],
 }
